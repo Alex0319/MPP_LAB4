b8bfdb95d408852567f9614abec1e44496236702
Alex0319 <alexandrsavchuk.97@gmail.com>
2016-10-23 18:59:45 +0300

    Add test target application

---
+++ AssemblyModificationApp/TestTarget/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTarget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<TestTargetClass> targetsList = new List<TestTargetClass>() { new TestTargetClass(), new TestTargetClass(15, "stringParam") };
+            for(int i=0;i<targetsList.Count;i++)
+            {
+                targetsList[i].SetFields(10+i*20,"newStringParam"+i);
+                targetsList[i].GetFields();
+            }
+        }
+    }
+}
---
+++ AssemblyModificationApp/TestTarget/TestTargetClass.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Logger;
+
+namespace TestTarget
+{
+    [Log]
+    public class TestTargetClass
+    {
+        private int classField1;
+        private string classField2;
+
+        public TestTargetClass()
+        {
+            classField1 = 0;
+            classField2 = null;
+        }
+
+        public TestTargetClass(int field1,string field2)
+        {
+            classField1 = field1;
+            classField2 = field2;
+        }
+
+        public Fields GetFields()
+        {
+            return new Fields(classField1,classField2);
+        }
+
+        public void SetFields(int field1, string field2)
+        {
+            classField1 = field1;
+            classField2 = field2;
+        }
+    }
+
+    public struct Fields
+    {
+        public Fields(int field1, string field2)
+        {
+            this.field1 = field1;
+            this.field2 = field2;
+        }
+
+        int field1;
+        string field2;
+    }
+}
