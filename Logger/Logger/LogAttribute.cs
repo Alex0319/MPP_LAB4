7e6c102ecf47914cb82722ae660372efb00e334d
Alex0319 <alexandrsavchuk.97@gmail.com>
2016-10-24 03:48:07 +0300

    Add to logger methods for logging returned classes

--- Logger/Logger/LogAttribute.cs
+++ Logger/Logger/LogAttribute.cs
@@ -7,42 +7,82 @@ using System.Threading;
 
 namespace Logger
 {
-    public class LogAttribute: Attribute
+    public class LogAttribute : Attribute
     {
-        public virtual void OnEnter(MethodBase method,Dictionary<string,object> parameters)
+        public virtual void OnEnter(MethodBase method, Dictionary<string, object> parameters)
         {
             WriteToFile(String.Format("CLASS: {{{0}}}. METHOD: {{{1}}}. PARAMETERS: {{{2}}}", method.DeclaringType.Name, method.Name, GetParameterValues(parameters)));
         }
 
         public virtual void OnExit(object returnValue)
         {
-            if(returnValue != null)
-                WriteToFile(String.Format(" and RETURNS {{{0}}}{1}",returnValue,Environment.NewLine));
+            if (returnValue != null)
+                WriteToFile(String.Format(" and RETURNS {{{0}}}{1}", GetStringWithReturnValue(returnValue), Environment.NewLine));
             else
-                WriteToFile(String.Format("{0}",Environment.NewLine));            
+                WriteToFile(String.Format("{0}", Environment.NewLine));
         }
 
         protected void WriteToFile(string logString)
         {
             using (var fileStream = new FileStream("FileLog.txt", FileMode.Append))
-                using (var streamWriter = new StreamWriter(fileStream))
-                {
-                    streamWriter.Write(logString);
-                    streamWriter.Flush();
-                }
+            using (var streamWriter = new StreamWriter(fileStream))
+            {
+                streamWriter.Flush();
+                streamWriter.Write(logString);
+            }
         }
 
         private string GetParameterValues(Dictionary<string, object> parameters)
         {
             const int countOfDeletedSymbols = 2;
-            StringBuilder stringBuilder=new StringBuilder();
+            StringBuilder stringBuilder = new StringBuilder();
             foreach (string parameter in parameters.Keys)
-                stringBuilder.AppendFormat("{0} = {1}, ",parameter,parameters[parameter]);
+                stringBuilder.AppendFormat("{0} = {1}, ", parameter, parameters[parameter]);
             if (stringBuilder.Length > 0)
                 stringBuilder.Remove(stringBuilder.Length - countOfDeletedSymbols, countOfDeletedSymbols);
             else
                 stringBuilder.Append("no params");
             return stringBuilder.ToString();
+        }
+
+        private string GetStringWithReturnValue(object returnValue)
+        {
+            Type returnValueType = returnValue.GetType();
+            if (returnValueType.IsValueType || returnValueType.IsClass)
+                return String.Format("{0}: {{{1}}}", returnValueType.ToString(), GetParameterValues(GetDictionaryFromReturnFieldsAndProperties(returnValue, returnValueType)));
+            if(returnValueType.IsEnum)
+                return String.Format("{0}: {{{1}}}", returnValueType.ToString(), GetParameterValues(GetDictionaryFromEnum(returnValue, returnValueType)));
+            return returnValue.ToString();
+        }
+
+        private Dictionary<string, object> GetDictionaryFromEnum(object returnValue, Type returnValueType)
+        {
+            var parameters = new Dictionary<string, object>();
+            string[] returnEnumNames = returnValueType.GetEnumNames();
+            Array returnEnumValues = returnValueType.GetEnumValues();
+            for (int i = 0; i < returnEnumNames.Length; i++)
+                parameters.Add(returnEnumNames[i], returnEnumValues.GetValue(i));
+            return parameters;
+        }
+
+        private Dictionary<string, object> GetDictionaryFromReturnFieldsAndProperties(object returnValue,Type returnValueType)
+        {
+            var parameters = new Dictionary<string, object>();
+            FieldInfo[] returnValueFields = returnValueType.GetFields(BindingFlags.Public
+                                                                      | BindingFlags.Instance
+                                                                      | BindingFlags.NonPublic
+                                                                      | BindingFlags.Static);
+            PropertyInfo[] returnValueProperties = returnValueType.GetProperties(BindingFlags.Public
+                                                                      | BindingFlags.Instance
+                                                                      | BindingFlags.NonPublic
+                                                                      | BindingFlags.Static);
+            int fieldsArrayLength = returnValueFields.Length, propertiesArrayLength = returnValueProperties.Length;
+            for (int i = 0; i < fieldsArrayLength + propertiesArrayLength; i++)
+                if (i < returnValueFields.Length)
+                    parameters.Add(returnValueFields[i].Name, returnValueFields[i].GetValue(returnValue));
+                else
+                    parameters.Add(returnValueProperties[i - fieldsArrayLength].Name, returnValueProperties[i - fieldsArrayLength].GetValue(returnValue));
+           return parameters;
         }
     }
 }
